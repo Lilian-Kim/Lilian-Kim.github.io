990590c31069c4380d065bcbb60f2509228f8a63
Lilian-Kim <lilian.k3104@gmail.com>
2024-04-27 17:07:41 +0900

    수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,8 @@ const repository = 'https://Lilian-Kim.github.io/'
 const nextConfig = {
   reactStrictMode: true,
   assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
-  trailingSlash: true, // 빌드 시 폴더 구조 그대로 생성하도록
+  trailingSlash: true, // 빌드시 폴더 구조 그대로 생성하도록
+  output: 'export',
 }
 
 export default nextConfig
