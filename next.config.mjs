93fb8f8412fb310895987a0cb17100ebb9504986
Lilian-Kim <lilian.k3104@gmail.com>
2024-04-27 17:49:19 +0900

    수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 
 const debug = process.env.NODE_ENV !== 'production'
-const repository = 'https://Lilian-Kim.github.io/'
+// const repository = '/'
 
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? `/${repository}/` : '', // production 일때 prefix 경로
+  assetPrefix: !debug ? `/` : '', // production 일때 prefix 경로
   trailingSlash: true, // 빌드시 폴더 구조 그대로 생성하도록
   output: 'export',
 }
